584fc9892a5b5f80ee90503a84cc9a4198e33ec5
kamila <kamila_zh@outlook.com>
2020-10-22 19:30:37 +0600

    добавлен Router и GET запрос

--- public/index.php
+++ public/index.php
@@ -1,9 +1,21 @@
 <?php
 
-use App\Framework\Kernel\Application;
+use Laminas\Diactoros\Response;
 use Laminas\Diactoros\ServerRequestFactory;
+use Laminas\HttpHandlerRunner\Emitter\SapiEmitter;
+use League\Route\Router;
 
 require_once __DIR__ . "/../vendor/autoload.php";
 
 $request = ServerRequestFactory::fromGlobals();
-Application::app($request)->start();
+
+$router = new Router;
+
+$router->get('/', function (){
+   $response = new Response();
+   $response->getBody()->write('Hello world!');
+   return $response;
+});
+
+$response = $router->dispatch($request);
+(new SapiEmitter)->emit($response);
